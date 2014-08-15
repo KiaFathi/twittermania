0690b7122b71737f0be9207e5d837945d8be3b02
Kia Fathi <kiafathi@me.com>
2014-08-14 20:55:32 -0700

    [FEATURE] Create basic text infrastructure

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+var app = require('express')();
+var http = require('http').Server(app);
+var port = process.env.port || 3000;
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+http.listen(port, function(){
+  console.log('listening on port: ' + port);
+});
